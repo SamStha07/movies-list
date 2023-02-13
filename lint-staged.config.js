51b52023b15c0aa8be48e4cd484da79dd37b5001
samstha07 <samshrestha85@gmail.com>
2023-02-13 23:20:15 +0545

    configured tailwindcss

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-dupe-keys */
 module.exports = {
 	// this will check Typescript files
 	'**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
 
 	// This will lint and format TypeScript and                                             //JavaScript files
-	'**/*.(ts|tsx|js)': (filenames) => [
+	'**/*.(ts|tsx)': (filenames) => [
 		`yarn eslint --fix ${filenames.join(' ')}`,
 		`yarn prettier --write ${filenames.join(' ')}`,
 	],
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 function App() {
 	const help = '12';
 
-	return <div>Hello World {help}</div>;
+	return (
+		<div>
+			Hello World {help}
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		</div>
+	);
 }
 
 export default App;
